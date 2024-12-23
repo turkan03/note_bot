f03214074d73da0b994e5e87f5418392074f5621
Turkan Suleimanova <turkan.suleimanovva@gmail.com>
2024-12-23 17:54:24 +0300

    Create a cursor object for database interaction

--- src/db.py
+++ src/db.py
@@ -6,3 +6,7 @@ def get_connection():
   connection = sqlite3.connect("my_data.db")
   return connection
 
+# Helper object that allows you to execute SQL commands and interact with the database
+#Вспомогательный объект, позволяющий выполнять команды SQL и взаимодействовать с базой данных
+cursor = connection.cursor()
+
