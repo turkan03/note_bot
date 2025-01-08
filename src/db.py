3d140293e9bff4a5b317ed95b2522cd0795070ea
Turkan Suleimanova <turkan.suleimanovva@gmail.com>
2025-01-08 21:34:39 +0300

    Creat tables for database

--- src/db.py
+++ src/db.py
@@ -10,3 +10,29 @@ def get_connection():
 #Вспомогательный объект, позволяющий выполнять команды SQL и взаимодействовать с базой данных
 cursor = connection.cursor()
 
+# Create table with user nickname or names 
+CREAT TABLE IF NOT EXIST users(
+  user_id INTEGER PRIMARY KEY,
+  usename TEXT NOT NULL
+)
+
+# Create table with user categories and connect it with user name
+CREAT TABLE IF NOT EXIST categories(
+  category_id INTEGER PRIMARY KEY,
+  category_name TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+
+)
+
+# Creat table for notes 
+CREAT TABLE IF NOT EXIST notes(
+  note_id INTEGER PRIMARY KEY,
+  note_title TEXT NOT NULL,
+  note_content TEXT,
+  user_id INTEGER NOT NULL,
+  category_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCE users(user_id),
+  FOREIGN KEY (category_id) REFERENCE categories(category_id)
+
+)
