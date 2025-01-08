bc4701f94654f33db6036db495b4d1f45c0af107
Turkan Suleimanova <turkan.suleimanovva@gmail.com>
2025-01-08 22:01:32 +0300

    Refactore database code

--- src/db.py
+++ src/db.py
@@ -6,33 +6,45 @@ def get_connection():
   connection = sqlite3.connect("my_data.db")
   return connection
 
-# Helper object that allows you to execute SQL commands and interact with the database
-#Вспомогательный объект, позволяющий выполнять команды SQL и взаимодействовать с базой данных
-cursor = connection.cursor()
+# Function to create tables in the database
+def initilizeDatabase():
+  conn = get_connection # Get the database connection
+  cursor = conn.cursor()
+  
+  # Create table with user nickname or names
+  cursor.execute('''
+    CREAT TABLE IF NOT EXIST users(
+      user_id INTEGER PRIMARY KEY,
+      usename TEXT NOT NULL
+    )
+  ''')
+  # Create table with user categories and connect it with user name
+  cursor.execute('''
+    CREAT TABLE IF NOT EXIST categories(
+      category_id INTEGER PRIMARY KEY,
+      category_name TEXT NOT NULL,
+      user_id INTEGER NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES users(user_id)
+    )
+  ''')
+  
+  # Creat table for notes 
+  cursor.execute('''
+    CREAT TABLE IF NOT EXIST notes(
+      note_id INTEGER PRIMARY KEY,
+      note_title TEXT NOT NULL,
+      note_content TEXT,
+      user_id INTEGER NOT NULL,
+      category_id INTEGER,
+      FOREIGN KEY (user_id) REFERENCE users(user_id),
+      FOREIGN KEY (category_id) REFERENCE categories(category_id)
+    )
+  ''')
 
-# Create table with user nickname or names 
-CREAT TABLE IF NOT EXIST users(
-  user_id INTEGER PRIMARY KEY,
-  usename TEXT NOT NULL
-)
+  # Commit changes and close the connection
+  conn.commit()
+  conn.close()
 
-# Create table with user categories and connect it with user name
-CREAT TABLE IF NOT EXIST categories(
-  category_id INTEGER PRIMARY KEY,
-  category_name TEXT NOT NULL,
-  user_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
 
-)
-
-# Creat table for notes 
-CREAT TABLE IF NOT EXIST notes(
-  note_id INTEGER PRIMARY KEY,
-  note_title TEXT NOT NULL,
-  note_content TEXT,
-  user_id INTEGER NOT NULL,
-  category_id INTEGER,
-  FOREIGN KEY (user_id) REFERENCE users(user_id),
-  FOREIGN KEY (category_id) REFERENCE categories(category_id)
-
-)
+# Call the function to create tables
+initilizeDatabase()
