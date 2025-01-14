68dcec960c25ab5cdc1ff4ab3486fb53d09acf6b
Turkan Suleimanova <turkan.suleimanovva@gmail.com>
2025-01-15 00:21:41 +0300

    Refactore for automatically close the connection

--- src/db.py
+++ src/db.py
@@ -1,59 +1,63 @@
 import sqlite3
 
-# Function to establish a connection to the database
-# Функция для установления соединения с базой данных
-def get_connection():
-  connection = sqlite3.connect("my_data.db")
-  return connection
+try:
+  with sqlite3.connect("my_data.db") as conn:
+    print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+except sqlite3.OperationalError as e:
+  print("Failed to open database:", e)
 
-# Function to create tables in the database
-def initilizeDatabase():
-  conn = get_connection # Get the database connection
-  cursor = conn.cursor()
+
+
+# # def get_connection():
+# #   connection = sqlite3.connect("my_data.db")
+# #   return connection
+
+# # Function to create tables in the database
+# # 
   
-  # Create table with user nickname or names
-  cursor.execute('''
-    CREAT TABLE IF NOT EXIST users(
-      user_id INTEGER PRIMARY KEY,
-      usename TEXT NOT NULL
-    )
-  ''')
-  # Create table with user categories and connect it with user name
-  cursor.execute('''
-    CREAT TABLE IF NOT EXIST categories(
-      category_id INTEGER PRIMARY KEY,
-      category_name TEXT NOT NULL,
-      user_id INTEGER NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES users(user_id)
-    )
-  ''')
+#   # Create table with user nickname or names
+#   cursor.execute('''
+#     CREAT TABLE IF NOT EXIST users(
+#       user_id INTEGER PRIMARY KEY,
+#       usename TEXT NOT NULL
+#     )
+#   ''')
+#   # Create table with user categories and connect it with user name
+#   cursor.execute('''
+#     CREAT TABLE IF NOT EXIST categories(
+#       category_id INTEGER PRIMARY KEY,
+#       category_name TEXT NOT NULL,
+#       user_id INTEGER NOT NULL,
+#       FOREIGN KEY (user_id) REFERENCES users(user_id)
+#     )
+#   ''')
   
-  # Creat table for notes 
-  cursor.execute('''
-    CREAT TABLE IF NOT EXIST notes(
-      note_id INTEGER PRIMARY KEY,
-      note_title TEXT NOT NULL,
-      note_content TEXT,
-      user_id INTEGER NOT NULL,
-      category_id INTEGER,
-      FOREIGN KEY (user_id) REFERENCE users(user_id),
-      FOREIGN KEY (category_id) REFERENCE categories(category_id)
-    )
-  ''')
+#   # Creat table for notes 
+#   cursor.execute('''
+#     CREAT TABLE IF NOT EXIST notes(
+#       note_id INTEGER PRIMARY KEY,
+#       note_title TEXT NOT NULL,
+#       note_content TEXT,
+#       user_id INTEGER NOT NULL,
+#       category_id INTEGER,
+#       FOREIGN KEY (user_id) REFERENCE users(user_id),
+#       FOREIGN KEY (category_id) REFERENCE categories(category_id)
+#     )
+#   ''')
 
-  # Commit changes and close the connection
-  conn.commit()
+#   # Commit changes and close the connection
+#   conn.commit()
   
-# Call the function to create tables
-initilizeDatabase()
+# # Call the function to create tables
+# initilizeDatabase()
 
-# Function to save user data into the table 
-def save_username(username):
-  conn = get_connection # Get the database connection
-  cursor = conn.cursor()
+# # Function to save user data into the table 
+# def save_username(username):
+#   conn = get_connection # Get the database connection
+#   cursor = conn.cursor()
 
-  cursor.execute('''
-  INSERT INTO users(username)
-  VALUE(?)''', (username))
-  conn.commit()
-  conn.close()
+#   cursor.execute('''
+#   INSERT INTO users(username)
+#   VALUE(?)''', (username))
+#   conn.commit()
+#   conn.close()
