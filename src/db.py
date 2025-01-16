f251be40ad797a78ae428a8e2e894ff0bd46e6ac
Turkan Suleimanova <turkan.suleimanovva@gmail.com>
2025-01-16 22:21:19 +0300

    Refactore code and add tables for database

--- src/db.py
+++ src/db.py
@@ -3,61 +3,41 @@ import sqlite3
 try:
   with sqlite3.connect("my_data.db") as conn:
     print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+      CREATE TABLE IF NOT EXISTS user(
+        user_id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL
+      );
+    ''')
+
+    cursor.execute('''
+      CREATE TABLE IF NOT EXISTS categories(
+        category_id INTEGER PRIMARY KEY,
+        category_name TEXT NOT NULL,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(user_id) 
+      );
+    ''')
+
+    cursor.execute('''
+      CREATE TABLE IF NOT EXISTS notes(
+      note_id INTEGER PRIMARY KEY,
+      note_title TEXT NOT NULL,
+      note_content TEXT,
+      user_id INTEGER NOT NULL,
+      category_id INTEGER,
+      FOREIGN KEY (user_id) REFERENCE user(user_id),
+      FOREIGN KEY (category_id) REFERENCE categories(category_id)
+      );
+    ''')
+    
+    conn.commit()
 except sqlite3.OperationalError as e:
   print("Failed to open database:", e)
 
 
 
-# # def get_connection():
-# #   connection = sqlite3.connect("my_data.db")
-# #   return connection
 
-# # Function to create tables in the database
-# # 
-  
-#   # Create table with user nickname or names
-#   cursor.execute('''
-#     CREAT TABLE IF NOT EXIST users(
-#       user_id INTEGER PRIMARY KEY,
-#       usename TEXT NOT NULL
-#     )
-#   ''')
-#   # Create table with user categories and connect it with user name
-#   cursor.execute('''
-#     CREAT TABLE IF NOT EXIST categories(
-#       category_id INTEGER PRIMARY KEY,
-#       category_name TEXT NOT NULL,
-#       user_id INTEGER NOT NULL,
-#       FOREIGN KEY (user_id) REFERENCES users(user_id)
-#     )
-#   ''')
-  
-#   # Creat table for notes 
-#   cursor.execute('''
-#     CREAT TABLE IF NOT EXIST notes(
-#       note_id INTEGER PRIMARY KEY,
-#       note_title TEXT NOT NULL,
-#       note_content TEXT,
-#       user_id INTEGER NOT NULL,
-#       category_id INTEGER,
-#       FOREIGN KEY (user_id) REFERENCE users(user_id),
-#       FOREIGN KEY (category_id) REFERENCE categories(category_id)
-#     )
-#   ''')
 
-#   # Commit changes and close the connection
-#   conn.commit()
-  
-# # Call the function to create tables
-# initilizeDatabase()
-
-# # Function to save user data into the table 
-# def save_username(username):
-#   conn = get_connection # Get the database connection
-#   cursor = conn.cursor()
-
-#   cursor.execute('''
-#   INSERT INTO users(username)
-#   VALUE(?)''', (username))
-#   conn.commit()
-#   conn.close()
